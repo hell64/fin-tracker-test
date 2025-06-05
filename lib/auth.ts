9b4e6db3383e0c6565bf2c623d7df37743bbb483
hell64 <hellbent105@gmail.com>
2025-06-05 19:22:28 +0300

    fix: pullic api url

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,30 +1,3 @@
-// import { cookies } from "next/headers";
-// import prisma from "@/lib/prisma";
-
-// export async function getUser() {
-//   const userId = (await cookies()).get("user_id")?.value;
-
-//   if (!userId) {
-//     return null;
-//   }
-
-//   try {
-//     const user = await prisma.user.findUnique({
-//       where: { id: Number(userId) },
-//       select: {
-//         id: true,
-//         name: true,
-//         email: true,
-//       },
-//     });
-
-//     return user;
-//   } catch (error) {
-//     console.error("Get user error:", error);
-//     return null;
-//   }
-// }
-
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
@@ -33,8 +6,9 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "sqlite", // or "mysql", "postgresql", ...etc
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/auth`,
   emailAndPassword: {
     enabled: true,
   },
